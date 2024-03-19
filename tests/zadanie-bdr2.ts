4f1f49ab56ed55ab992538be4182121b49771a8a
Bartlomiej <bartlomiejaz@gmail.com>
2024-03-20 00:39:32 +0100

    BDR2

--- tests/zadanie-bdr2.ts
+++ tests/zadanie-bdr2.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('https://demo.evershop.io/');
+
   await expect(page.locator('h2')).toContainText('Discount 20% For All Orders Over $2000');
+
   await page.getByRole('link', { name: 'Men', exact: true }).click();
+
   await page.getByRole('link', { name: 'Nizza trefoil shoes' }).first().click();
+
   await expect(page.locator('h1')).toContainText('Nizza trefoil shoes');
 });

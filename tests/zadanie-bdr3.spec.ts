9bfc52a201f939abcb4afb721aa1a3b797bf7489
Bartłomiej Adamowicz <bartlomiejaz@gmail.com>
2024-04-08 18:29:18 +0200

    BDR3

--- tests/zadanie-bdr3.spec.ts
+++ tests/zadanie-bdr3.spec.ts
@@ -6,18 +6,15 @@ async function goToEvershopPage(page: Page) {
     await expect(page.locator('.logo-icon')).toBeVisible();
 }
 
-
 async function goToKids(page: Page, MenuName: string, HeadingName: string ) {
     await page.getByRole('link', { name: MenuName }).click();
     await expect(page.getByRole('heading', { name: HeadingName })).toBeVisible();
 }
 
-
 async function goToShoesDetails(page: Page, ShoesName: string ) {
     await page.locator('a').filter({ hasText: /^Chuck taylor all star$/ }).click();
     //await page.getByRole('heading', { name: ShoesName }).click();
 }
-
 
 async function addToCartShoes(page: Page, ShoesSize: string, ShoesColor: string ) {
     await page.getByRole('link', { name: ShoesSize, exact: true }).click();
@@ -32,16 +29,13 @@ async function addToCartShoes(page: Page, ShoesSize: string, ShoesColor: string 
 
 }
 
-
 test('Dodanie butów dziecięcych do koszyka: M, kolor: red', async ({ page }) => {
   await goToEvershopPage(page)
   await goToKids(page, 'Shop kids', 'Kid')
   await goToShoesDetails(page, 'Chuck taylor all star')
   await addToCartShoes(page, 'M', 'Red')
 
-
 });
-
 
 test('Dodanie butów dziecięcych do koszyka: L, kolor: black', async ({ page }) => {
     await goToEvershopPage(page)
